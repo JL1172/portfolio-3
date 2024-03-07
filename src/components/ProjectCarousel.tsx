80f967668b45c912a89fedbf3cae880ccb5a407e
Jacob Lang <jacoblang72@comast.net>
2024-03-07 13:36:34 -0500

    need to finish responsiveness of slideshow

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -2,21 +2,22 @@ import "../styles/projects.css";
 import { projectData } from "../resources/projects-data";
 import { Carousel } from "flowbite-react";
 import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { Button } from "@mui/material";
 
 export const ProjectCarousel = () => {
-  const [visible, setVisible] = useState(0);
   const images = projectData;
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          const visibleIndex = Number(entry.target.className.match(/\d+/g));
-          setVisible(visibleIndex);
+          entry.target.classList.add("visible-span-title");
+        } else {
+          entry.target.classList.remove("visible-span-title");
         }
       });
     });
-    const hiddenSpan = document.querySelectorAll(".hidden-span");
+    const hiddenSpan = document.querySelectorAll(".hidden-span-title");
     hiddenSpan.forEach((element) => observer.observe(element));
   }, []);
   return (
@@ -24,26 +25,64 @@ export const ProjectCarousel = () => {
       <Carousel
         slide={false}
         className="w-full h-full"
-        leftControl={<BiLeftArrow />}
-        rightControl={<BiRightArrow />}
+        leftControl={
+          <BiLeftArrow
+            style={{ color: "#E0234E", width: "2rem", height: "2rem" }}
+          />
+        }
+        rightControl={
+          <BiRightArrow
+            style={{ color: "#E0234E", width: "2rem", height: "2rem" }}
+          />
+        }
       >
         {images.map((n, i) => {
           return (
             <div
-              className=" w-11/12  flex items-center max-h-fit h-80dvh"
+              className="xxs:w-full xs:w-full sm:w-full md:w-full lg:w-11/12 max-h-fit h-80dvh project-content"
               key={i}
             >
               <img src={n.img} alt={n.title} className="w-3/4 h-80dvh" />
-              <div className="w-full h-80dvh flex flex-col items-center bg-gray-800">
-                <div className="font-bold text-3xl mt-10">
-                  {/* 
-                  * need to finish styling and animations for content
-                  * need to make this page responsive
-                  * regarding responsiveness, consider stacking vertically or simply adding an option to touch the picture and then flip the picture
-                  * need to figure out typewriting animation
-                  * 
-                  */}
-                  {/* {n.title.split("").map((n,i)=>{<span key = {i}>{n}</span>})} */}
+              <div className="w-full  h-80dvh flex flex-col items-center justify-evenly bg-gray-800 rounded-md description">
+                <div className="font-bold flex flex-col items-center text-2xl mt-10 hidden-span-title w-11/12 mb-2">
+                  {/*
+                   * need to finish styling and animations for content
+                   * need to make this page responsive
+                   * regarding responsiveness, consider stacking vertically or simply adding an option to touch the picture and then flip the picture
+                   * need to figure out typewriting animation
+                   *
+                   */}
+                  {n.title}
+                <div className="text-lg text-gray-500 font-semibold mb-10">
+                  Category: {n.category}
+                </div>
+                </div>
+                <div className="w-11/12">
+                  {n.description}
+                </div>
+                <div className="text-2xl font-semibold flex flex-col justify-evenly items-center  w-11/12 h-1/2 mt-6">
+                   Technologies Used:
+                   <div className="flex justify-evenly flex-wrap h-3/4">
+                   {n.stack.map((icon,i) => {
+                    return <span className="mx-6" key = {i}>{icon}</span>
+                   })}
+                   </div>
+                </div>
+                <div className="w-11/12 flex items-center justify-center mb-10">
+                <Button
+                    className="font-bold lg:w-3/4 md:w-11/12 sm:w-11/12 xs:w-11/12 xxs:w-11/12 h-8"
+                    sx={{
+                      bgcolor: "royalblue",
+                      color: "black",
+                      fontSize: "12px",
+                      "&:hover": {
+                        bgcolor: "transparent",
+                      },
+                    }}
+                    variant="contained"
+                  >
+                    Explore {n.title}
+                  </Button>
                 </div>
               </div>
             </div>
--- src/resources/projects-data.tsx
+++ src/resources/projects-data.tsx
@@ -18,6 +18,7 @@ import { TbSql } from "react-icons/tb";
 import { FaNode, FaReact } from "react-icons/fa";
 import { IoLogoElectron } from "react-icons/io5";
 import { urlDict } from "./urls";
+import "../styles/projects.css";
 
 export const projectData = [
   {
@@ -28,14 +29,14 @@ export const projectData = [
     description:
       "Expense Tracker is a full-stack application designed to manage expenses efficiently, providing users with insightful financial analytics and controls.",
     stack: [
-      <SiExpress color="royalblue" />,
-      <BiLogoPostgresql color=" #0064a5" />,
-      <BsBootstrap color="#563d7c" />,
-      <TbSql color="orange" />,
-      <SiMui color="#757de8" />,
-      <FaReact color="rgb(97, 219, 251)" />,
-      <SiTailwindcss color="#6d28d9" />,
-      <IoLogoElectron color="royalblue" />,
+      <SiExpress color="royalblue" className="w-12 h-12" />,
+      <BiLogoPostgresql color=" #0064a5" className="w-12 h-12" />,
+      <BsBootstrap color="#563d7c" className="w-12 h-12" />,
+      <TbSql color="orange" className="w-12 h-12" />,
+      <SiMui color="#757de8" className="w-12 h-12" />,
+      <FaReact color="rgb(97, 219, 251)" className="w-12 h-12" />,
+      <SiTailwindcss color="#6d28d9" className="w-12 h-12" />,
+      <IoLogoElectron color="royalblue" className="w-12 h-12" />,
     ],
   },
   {
@@ -46,32 +47,32 @@ export const projectData = [
     description:
       "Node-Space is an online platform that allows developers to create creator accounts, write their own blogs, have their own following, and release their own courses.",
     stack: [
-      <SiNestjs color="#E0234E" />,
-      <BiLogoPostgresql color=" #0064a5" />,
-      <BsBootstrap color="#563d7c" />,
-      <SiPrisma color="#800000" />,
-      <SiMui color="#757de8" />,
-      <FaReact color="rgb(97, 219, 251)" />,
-      <SiTailwindcss color="#6d28d9" />,
-      <SiTypescript color="#007acc" />,
+      <SiNestjs color="#E0234E" className="w-12 h-12" />,
+      <BiLogoPostgresql color=" #0064a5" className="w-12 h-12" />,
+      <BsBootstrap color="#563d7c" className="w-12 h-12" />,
+      <SiPrisma color="#800000" className="w-12 h-12" />,
+      <SiMui color="#757de8" className="w-12 h-12" />,
+      <FaReact color="rgb(97, 219, 251)" className="w-12 h-12" />,
+      <SiTailwindcss color="#6d28d9" className="w-12 h-12" />,
+      <SiTypescript color="#007acc" className="w-12 h-12" />,
     ],
   },
   {
-    title: "NPM Manager/React Initializer",
+    title: "NPM Manager",
     category: "Full Stack",
     linkedinUrl: urlDict.npmManagerLinkedInUrl,
     img: npmManagerPng,
     description:
       "Developed an Electron application to automate the dynamic startup of Node.js applications located in different directories, significantly improving development efficiency: used child-process module in node. Engineered a feature, that, with simple configuration, and one-click of a button, instantiates a react application and downloads the checked NPM packages in a chosen directory, which streamlines project configuration and setup.",
     stack: [
-      <SiExpress color="royalblue" />,
-      <SiStyledcomponents color="purple" />,
-      <BsBootstrap color="#563d7c" />,
-      <TbSql color="orange" />,
-      <SiMui color="#757de8" />,
-      <FaReact color="rgb(97, 219, 251)" />,
-      <IoLogoElectron color="royalblue" />,
-      <FaNode color="#68a063" />,
+      <SiExpress color="royalblue" className="w-12 h-12" />,
+      <SiStyledcomponents color="purple" className="w-12 h-12" />,
+      <BsBootstrap color="#563d7c" className="w-12 h-12" />,
+      <TbSql color="orange" className="w-12 h-12" />,
+      <SiMui color="#757de8" className="w-12 h-12" />,
+      <FaReact color="rgb(97, 219, 251)" className="w-12 h-12" />,
+      <IoLogoElectron color="royalblue" className="w-12 h-12" />,
+      <FaNode color="#68a063" className="w-12 h-12" />,
     ],
   },
   {
@@ -81,7 +82,7 @@ export const projectData = [
     img: schemaCrafter,
     description:
       "SchemaCrafterJs is a lightweight and strongly-typed form validation package designed for use with TypeScript. It allows you to define schemas for your form fields and validate payloads against these schemas. This package has one dependencies and no dependents, making it easy to integrate into your projects. I made this NPM package because I wanted something catered to my use cases and I often did not like one or more features of other form-validation libraries, therefore I made something that solves some of the annoying issues I found to be apparent in other schema-building packages.",
-    stack: [<FaNode color="#68a063" />, <SiTypescript color="#007acc" />],
+    stack: [<FaNode color="#68a063" className="w-12 h-12"/>, <SiTypescript color="#007acc" className="w-12 h-12"/>],
   },
   {
     title: "Node Space RESTful API",
@@ -91,11 +92,11 @@ export const projectData = [
     description:
       "This RESTful API is a production API hosted on render utilizing MVC architecure to maintain scalability and robustness. This API is used as the main means of authentication and authorization. It has many security features built in, such as rate-limiting, ip blacklisting, jwt management, input sanitation and validation, image processing and scanning, and more.",
     stack: [
-      <FaNode color="#68a063" />,
-      <SiTypescript color="#007acc" />,
-      <SiNestjs color="#E0234E" />,
-      <BiLogoPostgresql color=" #0064a5" />,
-      <SiPrisma color="#800000" />,
+      <FaNode color="#68a063" className="w-12 h-12"/>,
+      <SiTypescript color="#007acc" className="w-12 h-12"/>,
+      <SiNestjs color="#E0234E" className="w-12 h-12"/>,
+      <BiLogoPostgresql color=" #0064a5" className="w-12 h-12"/>,
+      <SiPrisma color="#800000" className="w-12 h-12"/>,
     ],
   },
 ];
