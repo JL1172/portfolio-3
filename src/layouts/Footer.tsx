42d084165c2b9d6303806cc3da7f96a94721d5d2
Jacob Lang <jacoblang72@comast.net>
2024-03-08 10:58:17 -0500

    footer is done

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,45 +1,110 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "../styles/footer.css";
-import { MdEmail } from "react-icons/md";
+import { MdEmail, MdLibraryBooks } from "react-icons/md";
 import { urlDict } from "../resources/urls";
+import { FcAbout } from "react-icons/fc";
+import { RiStackFill } from "react-icons/ri";
+import { GoProjectSymlink } from "react-icons/go";
+import { IoIosContact } from "react-icons/io";
+import { BiHome } from "react-icons/bi";
 const contactMeData = [
-  { icon: <FaLinkedin  color="dodgerblue" className="h-8 w-8"/>, url: urlDict.linkedin },
-  { icon: <FaGithub color="dodgerblue" className="h-8 w-8"/>, url: urlDict.github },
   {
-    icon: <MdEmail color="dodgerblue" className="h-8 w-8"/>,
+    icon: <FaLinkedin color="dodgerblue" className="h-8 w-8" />,
+    url: urlDict.linkedin,
+  },
+  {
+    icon: <FaGithub color="dodgerblue" className="h-8 w-8" />,
+    url: urlDict.github,
+  },
+  {
+    icon: <MdEmail color="dodgerblue" className="h-8 w-8" />,
     url: "mailto:jacoblang127@gmail.com",
   },
 ];
-//TODO have to fix this page
+const navData = [
+  {
+    icon: <FcAbout color="dodgerblue" className="h-8 w-8" />,
+    text: "About Me",
+    url: "about",
+  },
+  {
+    icon: <RiStackFill color="dodgerblue" className="h-8 w-8" />,
+    text: "Tech Stack",
+    url: "techstack",
+  },
+  {
+    icon: <GoProjectSymlink color="dodgerblue" className="h-8 w-8" />,
+    text: "Projects",
+    url: "projects",
+  },
+  {
+    icon: <MdLibraryBooks color="dodgerblue" className="h-8 w-8" />,
+    text: "Resume",
+    url: "resume",
+  },
+  {
+    icon: <IoIosContact color="dodgerblue" className="h-8 w-8" />,
+    text: "Contact",
+    url: "contact",
+  },
+];
 export default function Footer() {
   return (
-    <div className="text-white w-full h-96 bg-color-custom-gray-footer flex justify-evenly items-center">
-      <div className="flex flex-col basis-1/2  h-1/2">
-        <div className="text-2xl font-bold mb-6">Socials</div>
-        <div className="flex w-full ">
-          {contactMeData.map((n, i) => {
-            return (
-              <a
-                className="hover:scale-105 hover:transition-transform transition-transform mr-6 "
-                target="_blank"
-                key={i}
-                href={n.url}
-              >
-                {n.icon}
-              </a>
-            );
-          })}
+    <div className="text-white w-full h-50dvh-footer bg-color-custom-gray-footer flex flex-col justify-evenly outline-custom-footer items-center">
+      <div className="w-full flex justify-evenly h-custom">
+        <div className="flex flex-col items-center basis-1/2 h-full border-right-custom">
+          <div className="text-2xl font-bold">Socials</div>
+          <div className="flex flex-col h-full w-full justify-evenly items-center">
+            {contactMeData.map((n, i) => {
+              return (
+                <a
+                  className="hover:scale-105 hover:transition-transform transition-transform w-fit"
+                  target="_blank"
+                  key={i}
+                  href={n.url}
+                >
+                  {n.icon}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col basis-1/2  h-full items-center justify-evenly">
+          <div className="text-2xl font-bold">Navigation</div>
+          <div className="flex flex-col h-full justify-evenly items-baseline">
+            {navData.map((n, i) => {
+              return (
+                <a
+                  className="hover:brightness-75 hover:transition-color transition-color w-fit flex text-gray-200 md:font-semibold text-sm items-center"
+                  key={i}
+                  href={n.url}
+                >
+                  {n.icon}
+                  {n.text}
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
-      <div className="flex flex-col w-1/6 h-1/2 first:justify-evenly basis-1/2 " >
-        <div className="text-2xl font-bold">Directory</div>
+      <div className="flex w-full justify-evenly mt-2 mb-2">
+        <a className="text-sm font-bold hover:brightness-75" href={urlDict.thisRepo}>
+          View This Repo
+        </a>
+        <a className="text-sm font-bold flex items-center hover:brightness-75" href="#home">
+          <BiHome />
+          Back To Top{" "}
+        </a>
+      </div>
+      {/* <div className="" >
+        <div className="text-2xl font-bold">Navigation</div>
         <a href="#home">Top of Page</a>
         <a href="#about">About Me</a>
         <a href="#techstack">Tech Stack</a>
         <a href="#projects">Projects</a>
         <a href="#resume">Resume</a>
         <a href="#contact">Contact Me</a>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/resources/urls.ts
+++ src/resources/urls.ts
@@ -7,8 +7,9 @@ export const urlDict = {
     "https://www.linkedin.com/posts/jacob-lang1172_react-nodejs-expressjs-activity-7130240165989507073-aLIs/?utm_source=share&utm_medium=member_desktop&controls=0",
   nodeSpaceGithubUrl: "https://github.com/stars/JL1172/lists/node-space",
   expenseTrackerGithubUrl: "https://github.com/JL1172/Expense-Tracker",
-  npmPackageGithubUrl:"https://github.com/stars/JL1172/lists/npm-packages",
-  nodeSpaceBackend:"https://github.com/JL1172/node-space-backend",
+  npmPackageGithubUrl: "https://github.com/stars/JL1172/lists/npm-packages",
+  nodeSpaceBackend: "https://github.com/JL1172/node-space-backend",
   authenticationMicroservice: "https://github.com/JL1172/auth-microservice-api",
   resume: "https://1drv.ms/w/s!AgsJlcR3TIuWwqVvjZEoDXRGacKrrA?e=Ha1VFy",
+  thisRepo: "https://github.com/JL1172/portfolio-3",
 };
